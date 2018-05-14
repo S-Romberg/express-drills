7ccaf4f2b59ff04dbacc90a01939af937e8379df
S Romberg <SRomberg@Macbook-Pro.local>
2018-05-14 15:42:11 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+var port = process.env.PORT || 3000
+
+var cohorts = [{
+    id: 1,
+    cohortName: 'g100',
+    cohortCode: '17-01-WD-DP',
+    studentNum: 28
+},
+{
+    id: 2,
+    cohortName: 'g105',
+    cohortCode: '17-01-DS-GT',
+    studentNum: 24
+},
+{
+    id: 3,
+    cohortName: 'g109',
+    cohortCode: '17-02-WD-PX',
+    studentNum: 30
+},
+{
+    id: 4,
+    cohortName: 'g110',
+    cohortCode: '17-03-WD-BD',
+    studentNum: 29
+},
+]
+
+function findById(data, id){
+    for(let i = 0; i<data.length; i++){
+        if(data[i].id == id){
+            return data[i]
+        }
+    }
+    return null
+}
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.json({
+        data: cohorts
+    })
+})
+
+app.get('/:id', ((req, res) => {
+    var record = findById(cohorts, request.params.id)
+    if(!record){
+        response.status = 404;
+        response.json({
+            error: {
+                message: 'No record found!'
+            }
+        })
+    }
+    response.json({data: record})
+}))
+
+app.listen(port)
